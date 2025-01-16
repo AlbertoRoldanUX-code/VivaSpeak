73f2daaa37c962e1532ba5af2985ea4a4c7ddfec
AlbertoRoldanUX-code <albe.roldan@outlook.com>
2025-01-16 13:04:49 +0100

    Refactor HTML and CSS: Improved structure, responsiveness, and styling. Added a dynamic home section with CTA buttons and refined navbar functionality for mobile view.

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-"use strict";
+$(document).ready(function () {
+  // Navbar elements:
+  const menu = $("#nav-toggle");
+  const dropdown = $("nav ul");
+  const links = $(".nav-list");
 
-//Navbar elements:
-const menu = $("#nav-toggle");
-const dropdown = $("nav ul");
-const links = $(".nav-list");
+  // Ensure dropdown is hidden on mobile view on page load
+  if ($(window).width() <= 768) {
+    dropdown.hide();
+  }
 
-//Opens dropdown when clicking the menu
-menu.click(function () {
-  dropdown.slideToggle();
-  this.classList.toggle("active");
+  // Opens dropdown when clicking the menu
+  menu.click(function () {
+    dropdown.slideToggle();
+    this.classList.toggle("active");
+  });
+
+  // Closes dropdown when clicking on a link in mobile view only
+  $(".nav-list a").click(function () {
+    if (window.innerWidth <= 768) {
+      if (dropdown.is(":visible")) {
+        menu[0].classList.toggle("active");
+        dropdown.slideUp();
+      }
+    }
+  });
+
+  // Makes year dynamic
+  $("#year").html(new Date().getFullYear());
 });
-
-//Closes dropdown when clicking an option
-links.click(function () {
-  menu[0].classList.toggle("active");
-  dropdown.slideUp();
-});
-
-//Makes year dynamic
-$("#year").html(new Date().getFullYear());
