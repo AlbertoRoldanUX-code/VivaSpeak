4ed2da18a953561cdedbb68ffe1ca63a349b170a
AlbertoRoldanUX-code <albe.roldan@outlook.com>
2025-01-20 03:28:21 +0100

    Refactor dropdown behavior and icon toggle for mobile navigation

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 $(document).ready(function () {
-  // Navbar elements:
   const menu = $("#hamburger");
   const dropdown = $("nav ul");
 
-  // Ensure dropdown is hidden on mobile view on page load
   $(window)
     .on("resize", function () {
       if ($(window).width() > 768) {
         dropdown.show(); // Asegura que el dropdown está siempre visible en desktop
+        menu.removeClass("active"); // Asegura que el icono esté en estado hamburguesa
       } else {
         dropdown.hide(); // Esconde el dropdown en vistas móviles inicialmente
+        menu.removeClass("active"); // Asegura que el icono esté en estado hamburguesa
       }
     })
     .trigger("resize"); // Trigger the resize event on page load
 
-  // Opens dropdown when clicking the menu
   menu.click(function () {
     dropdown.slideToggle();
-    this.classList.toggle("active");
+    this.classList.toggle("active"); // Cambia el estado de activo a inactivo y viceversa
   });
 
-  // Closes dropdown when clicking on a link in mobile view only
   $(".nav-list a").click(function () {
     if ($(window).width() <= 768) {
       if (dropdown.is(":visible")) {
-        menu[0].classList.toggle("active");
+        menu.removeClass("active");
         dropdown.slideUp();
       }
     }
   });
-
   // Makes year dynamic
   $("#year").text(new Date().getFullYear());
 });
